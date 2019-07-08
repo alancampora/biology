928d36638047cee56765d3cd2ca2034a6c08058c
Alan Campora <alan.campora@gmail.com>
2019-07-07 21:00:31 -0300

    refactor(dna-rna-protein): remove unncessary files

--- packages/dna-rna-protein/src/components/chain-drawer/styled.js
+++
@@ -1,6 +0,0 @@
-import styled from 'styled-components';
-
-export const StyledDNAWrapper = styled.div`
-	display: grid;
-	margin: 1rem;
-`;
--- packages/dna-rna-protein/src/components/draw-dna/styled.js
+++
@@ -1,5 +0,0 @@
-import styled from 'styled-components';
-
-export const StyledDNAWrapper = styled.div`
-	display: grid;
-`;
